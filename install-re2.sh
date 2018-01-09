fed04f09413cbb31d0655ade636d25d4d07b3fb1
Andrey Sverdlichenko <blaze@ruddy.ru>
2018-01-08 20:09:37 -0800

    Ensure re2 download

--- install-re2.sh
+++ install-re2.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -ex
 
-TARBALL=`curl -s https://api.github.com/repos/google/re2/releases/latest | jq -r ".tarball_url"`
+TARBALL=`curl https://api.github.com/repos/google/re2/releases/latest | jq -r ".tarball_url"`
+if [ ${TARBALL} = "null" ] ; then exit 1 ; fi
 curl -L -o source.tar.gz ${TARBALL}
 tar -xzvf source.tar.gz
 mv google-re2-* google-re2
